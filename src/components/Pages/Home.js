c8025c01c2d85d97f195107ff8d053deb5d0f510
Tim Hurd <tim@interactivetools.com>
2025-04-18 16:31:58 -0700

    Added call to action handler for home and renamed handler for cards.

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -6,8 +6,12 @@ import seafood from "../../assets/seafood-lunch.png";
 import Card from "../Card/Card";
 
 const Home = () => {
-    const buttonClickHandler = () => {
+    const cardButtonClickHandler = () => {
         window.location.href = "/menu";
+    }
+
+    const ctaClickHandler = () => {
+        window.location.href = "/reservations";
     }
 
     return (
@@ -19,14 +23,14 @@ const Home = () => {
                     
                     <p>We are a family owned Mediterranean restaurant, located on Maldove Street in Chicago, Illionis. We focus on traditional recipes served with a modern twist.</p>
 
-                    <button className="btn-primary">Reserve a Table</button>
+                    <button className="btn-primary" onClick={ctaClickHandler}>Reserve a Table</button>
                 </div>
                 <div className="home-image">
                     <img src={homesplash} alt="Little Lemon Home" className="home-dish"/>
                 </div>
             </section>
             <section className="main-body">
-                <h3>Our Highlights</h3>
+                <h3>OUR HIGHLIGHTS</h3>
 
                 <div className="card-list">
                     <Card 
@@ -34,34 +38,34 @@ const Home = () => {
                         title="Delicous Bruchetta"    
                         description="Enjoy the delicious fresh taste of our bruchetta! Made with only ripe tomatoes and our family's olive oil from Italy, you can't go wrong with this dish."
                         buttonText="Visit our menu"
-                        onButtonClick={buttonClickHandler}
+                        onButtonClick={cardButtonClickHandler}
                     />
                     <Card 
                         imageSrc={greek}
                         title="Greek Salad"    
                         description="A refreshing salad made with fresh tomatoes, cucumbers, peppers, onions, olives, and feta cheese. This salad is perfect for any time of the day."
                         buttonText="Visit our menu"
-                        onButtonClick={buttonClickHandler}
+                        onButtonClick={cardButtonClickHandler}
                     />
                     <Card 
                         imageSrc={pasta}
                         title="Penne Marinara"    
                         description="All of our pasta is made in house. This classic penne is to die for and we are sure you will love it."
                         buttonText="Visit our menu"
-                        onButtonClick={buttonClickHandler}
+                        onButtonClick={cardButtonClickHandler}
                     />
                     <Card 
                         imageSrc={seafood}
                         title="Seafood Spaghetti"    
                         description="Any seafood lovers out there? This is the perfect dish for you! Our seafood is fresh and cooked to perfection."
                         buttonText="Visit our menu"
-                        onButtonClick={buttonClickHandler}
+                        onButtonClick={cardButtonClickHandler}
                     />
                 </div>
             </section>
 
             <section className="testimonial-container">
-                <h3>Testimonials</h3>
+                <h3>TESTIMONIALS</h3>
                 <p>"The food was amazing and the service was great! I can't wait to come back!"</p>
             </section>
         </main>
