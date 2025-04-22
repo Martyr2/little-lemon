39b8c96296639b6c753e0d49440b9391e639bf0e
Tim Hurd <martyr2@gmail.com>
2025-04-22 11:39:40 -0700

    Added content for the confirmation page.

--- src/components/Pages/Confirmation.js
+++ src/components/Pages/Confirmation.js
@@ -1,8 +1,26 @@
+import { Link } from "react-router";
+import HeroImage from "../HeroImage/HeroImage";
+
+// Image assets
+import confirmation from "../../assets/fireworks.jpg";
+
 const Confirmation = () => {
     return (
         <main>
+            <HeroImage image={confirmation}>
+                <h1 className="shaded-heading">Confirmation</h1>
+            </HeroImage>
             <section className="main-body">
-                <h1>Confirmation</h1>
+                <div className="confirmation-container">
+                    <h3>Your Reservation has been Confirmed!</h3>
+                    <p>Details about your confirmation has been sent to you by email.</p>
+                    <p>Thanks for booking with us. We will see you soon!</p>
+                </div>
+
+                <div className="confirmation-buttons">
+                    <Link to="/"><button className="btn-primary">Home</button></Link>
+                    <Link to="/menu"><button className="btn-primary">Our Menu</button></Link>
+                </div>
             </section>
         </main>
     )
