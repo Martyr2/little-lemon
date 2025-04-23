a3a770a28547f825925e4455f545b0ae2c6e22de
Tim Hurd <martyr2@gmail.com>
2025-04-23 10:53:20 -0700

    Added react testing library tests for updateTimes and initialization of times.

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,44 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { MemoryRouter } from "react-router";
 import Reservation from './components/Pages/Reservation';
 
 test('Renders the Reservation heading', () => {
-  render(<Reservation />);
-  const headingElement = screen.getByText("Book Table");
-  expect(headingElement).toBeInTheDocument();
+  render(
+    <MemoryRouter>
+      <Reservation />
+    </MemoryRouter>
+  );
+
+  const button = screen.getByRole("button", { name: /book table/i });
+  expect(button).toBeInTheDocument();
 });
 
-// test('Testing the initializeTimes', () => {
-//   render(<Reservation />);
-//   const headingElement = screen.getByText("Book Now");
-//   expect(headingElement).toBeInTheDocument();
-// })
+test('Testing the initialization of times', async () => {
+  render(
+    <MemoryRouter>
+      <Reservation />
+    </MemoryRouter>
+  );
 
-// test('Testing the updateTimes', () => {
-//   render(<Reservation />);
-//   const headingElement = screen.getByText("Book Now");
-//   expect(headingElement).toBeInTheDocument();
-// })
+  // New times from should appear
+  expect(await screen.findByText("17:00")).toBeInTheDocument();
+});
+
+
+test('Testing the updateTimes', async () => {
+  render(
+    <MemoryRouter>
+      <Reservation />
+    </MemoryRouter>
+  );
+
+  // Grab the date input
+  const dateInput = screen.getByLabelText(/Select Date/i);
+
+  // Simulate picking May 1, 2026
+  fireEvent.change(dateInput, { target: { value: "05/01/2026" } });
+
+  // New times from should appear
+  expect(await screen.findByText("19:00")).toBeInTheDocument();
+  expect(await screen.findByText("21:30")).toBeInTheDocument();
+});
